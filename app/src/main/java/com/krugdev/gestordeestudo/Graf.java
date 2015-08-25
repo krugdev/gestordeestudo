90b02ec4a988168963bcf24bec07bb47e01d335b
krugdev <andrekrueger.ene@gmail.com>
2015-08-25 11:08:28 -0300

    ok

--- app/src/main/java/com/krugdev/gestordeestudo/Graf.java
+++ app/src/main/java/com/krugdev/gestordeestudo/Graf.java
@@ -5,10 +5,14 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.RectF;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.ArcShape;
 import android.util.AttributeSet;
 import android.view.View;
+import android.graphics.Color;
+
+import java.util.ArrayList;
 
 /**
  * Created by Krug on 24/08/2015.
@@ -17,28 +21,23 @@ public class Graf extends View {
 
     //circle and text colors
     private int circleCol, labelCol;
+
     //label text
     private String circleText;
+
     //paint for drawing custom view
     private Paint ArcPaint;
 
-    private ArcShape Arc[];
-
     private int qtd;
 
-    private int cor[];
+    private ArrayList angulo;
 
-    private ShapeDrawable Shape;
-
-
+    private ArrayList cor;
 
     public Graf(Context context, AttributeSet attrs){
         super(context, attrs);
 
         //paint object for drawing in onDraw
-        ArcPaint = new Paint();
-
-        Shape = new ShapeDrawable();
 
         //get the attributes specified in attrs.xml using the name we included
         TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.Graf, 0, 0);
@@ -48,44 +47,39 @@ public class Graf extends View {
             circleText = a.getString(R.styleable.Graf_circleLabel);
             circleCol = a.getInteger(R.styleable.Graf_circleColor, 0);//0 is default
             labelCol = a.getInteger(R.styleable.Graf_labelColor, 0);
-            cor[0]=a.getInteger(R.styleable.Graf_sliceColor_0, 0);
-            cor[1]=a.getInteger(R.styleable.Graf_sliceColor_1, 0);
-            cor[2]=a.getInteger(R.styleable.Graf_sliceColor_2, 0);
-            cor[3]=a.getInteger(R.styleable.Graf_sliceColor_3, 0);
-            cor[4]=a.getInteger(R.styleable.Graf_sliceColor_4, 0);
-            cor[5]=a.getInteger(R.styleable.Graf_sliceColor_5, 0);
-            cor[6]=a.getInteger(R.styleable.Graf_sliceColor_6, 0);
-            cor[7]=a.getInteger(R.styleable.Graf_sliceColor_7, 0);
-            cor[8]=a.getInteger(R.styleable.Graf_sliceColor_8, 0);
-            cor[9]=a.getInteger(R.styleable.Graf_sliceColor_9, 0);
-            cor[10]=a.getInteger(R.styleable.Graf_sliceColor_10, 0);
-            cor[11]=a.getInteger(R.styleable.Graf_sliceColor_11, 0);
-            cor[12]=a.getInteger(R.styleable.Graf_sliceColor_12, 0);
-            cor[13]=a.getInteger(R.styleable.Graf_sliceColor_13, 0);
-            cor[14]=a.getInteger(R.styleable.Graf_sliceColor_14, 0);
-            cor[15]=a.getInteger(R.styleable.Graf_sliceColor_15, 0);
-            cor[16]=a.getInteger(R.styleable.Graf_sliceColor_16, 0);
-            cor[17]=a.getInteger(R.styleable.Graf_sliceColor_17, 0);
-            cor[18]=a.getInteger(R.styleable.Graf_sliceColor_18, 0);
-            cor[19]=a.getInteger(R.styleable.Graf_sliceColor_19, 0);
+
 
 
         } finally {
             a.recycle();
         }
 
+        ArcPaint = new Paint();
+        ArcPaint.setStyle(Style.STROKE);
+        ArcPaint.setAntiAlias(true);
+        ArcPaint.setStrokeWidth(10);
+        qtd = 0;
+        angulo = new ArrayList();
+        angulo.clear();
+        cor = new ArrayList();
+        cor.clear();
 
+
+        /*for(int i=0 ; i < qtd ; i++){
+            Arc[i] = new ArcShape(angulo[i],angulo[i+1]);
+            Arc[i].s
+            Shape[i] = new ShapeDrawable(Arc[i]);
+            ArcPaint[i] = new Paint();
+            ArcPaint[i].setStyle(Style.FILL);
+            ArcPaint[i].setAntiAlias(true);
+            ArcPaint[i].setColor(cor[i]);
+            ArcPaint[i].setStrokeWidth(10);
+            Shape[i].getPaint().set(ArcPaint[i]);
+        }*/
 
 
     }
 
-    public void angulo(int a[]) {
-
-        for(int i=0 ; i < 20 ; i++){
-            Arc[i] = new ArcShape(a[i],a[i+1]);
-        }
-
-    }
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -103,41 +97,21 @@ public class Graf extends View {
         else
             radius=viewWidthHalf-10;
 
-        ArcPaint.setStyle(Style.FILL);
-        ArcPaint.setAntiAlias(true);
+        int anguloInicial = 0;
 
-        //set the paint color using the circle color specified
-        ArcPaint.setColor(circleCol);
-
-
-        Shape.setBounds(10, 10, radius * 2, radius * 2);
-
-
-
-        for(int i=0 ; i < 20 ; i++){
-            Shape.setShape(Arc[i]);
-            ArcPaint.setColor(cor[i]);
-            Shape.getPaint().set(ArcPaint);
-            Shape.draw(canvas);
+        for(int i=0 ; i < angulo.size()  ; i++){
+            ArcPaint.setColor((int) cor.get(i));
+            canvas.drawArc(10, 10, radius * 2, radius * 2, anguloInicial, (float) angulo.get(i), false,ArcPaint);
         }
-
-
-
-        //canvas.drawCircle(viewWidthHalf, viewHeightHalf, radius, circlePaint);
-
-        //set the text color using the color specified
-        //ArcPaint.setColor(labelCol);
-
-        //set text properties
-        //ArcPaint.setTextAlign(Paint.Align.CENTER);
-        //ArcPaint.setTextSize(50);
-
-        //draw the text using the string attribute and chosen properties
-        //canvas.drawText(circleText, viewWidthHalf, viewHeightHalf, circlePaint);
 
 
     }
 
+
+    public void set(ArrayList c,ArrayList a){
+        cor = c;
+        angulo = a;
+    }
 
     public int getCircleColor(){
         return circleCol;
--- app/src/main/java/com/krugdev/gestordeestudo/Grafico.java
+++ app/src/main/java/com/krugdev/gestordeestudo/Grafico.java
@@ -7,18 +7,49 @@ import android.view.MenuItem;
 import android.graphics.Color;
 import android.view.View;
 
+import java.util.ArrayList;
+
 
 public class Grafico extends Activity {
 
     private Graf Graf;
-    private int angulo[]={10,25,45,60,71,80,98,125,160,230,250,360,360,360,360,360,360,360,360,360};
+    private ArrayList angulo;
+    private ArrayList cor;
+    private Color conversor;
+            //= {10,25,45,60,71,80,98,125,160,230,250,360,360,360,360,360,360,360,360,360};
+
+    //angulo[20] = {10,25,45,60,71,80,98,125,160,230,250,360,360,360,360,360,360,360,360,360};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grafico);
         Graf = (Graf)findViewById(R.id.Graf);
-        Graf.angulo(angulo);
+
+        angulo = new ArrayList();
+        angulo.clear();
+        angulo.add(10);
+        angulo.add(25);
+        angulo.add(40);
+        angulo.add(35);
+        angulo.add(12);
+        angulo.add(45);
+        angulo.add(25);
+
+        conversor = new Color();
+
+        cor = new ArrayList();
+        cor.clear();
+        cor.add(conversor.argb(0,255,0,0));
+        cor.add(conversor.argb(0,255,255,0));
+        cor.add(conversor.argb(0,255,0,255));
+        cor.add(conversor.argb(0,0,255,0));
+        cor.add(conversor.argb(0,0,255,255));
+        cor.add(conversor.argb(0,255,255,255));
+        cor.add(conversor.argb(0,0,0,255));
+
+        Graf.set(cor,angulo);
+
     }
 
     @Override
@@ -43,10 +74,12 @@ public class Grafico extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void btnPressed(View view){
+    public void btnPressed(View view) {
         //update the View
         Graf.setCircleColor(Color.GREEN);
         Graf.setLabelColor(Color.MAGENTA);
         Graf.setLabelText("Help");
+        Graf.set();
+        Graf.invalidate();
     }
 }
