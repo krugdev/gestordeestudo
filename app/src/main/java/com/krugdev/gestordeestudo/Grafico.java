cf82411538814579222a29b512555297b274c225
krugdev <andrekrueger.ene@gmail.com>
2015-08-26 17:24:08 -0300

    ok

--- app/src/main/java/com/krugdev/gestordeestudo/Grafico.java
+++ app/src/main/java/com/krugdev/gestordeestudo/Grafico.java
@@ -2,10 +2,14 @@ package com.krugdev.gestordeestudo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.graphics.Color;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,24 +19,49 @@ public class Grafico extends Activity {
     private Graf Graf;
     private ArrayList<Float> angulo;
     private ArrayList<Integer> cor;
-    //private Color conversor;
-            //= {10,25,45,60,71,80,98,125,160,230,250,360,360,360,360,360,360,360,360,360};
 
-    //angulo[20] = {10,25,45,60,71,80,98,125,160,230,250,360,360,360,360,360,360,360,360,360};
+    private TextView textTimer;
+    private Button startButton;
+    private Button pauseButton;
+    private long startTime = 0L;
+    private Handler myHandler = new Handler();
+    long timeInMillies = 0L;
+    long timeSwap = 0L;
+    long finalTime = 0L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grafico);
+
         Graf = (Graf)findViewById(R.id.Graf);
 
         angulo = new ArrayList<Float>();
         angulo.clear();
 
-        //conversor = new Color();
-
         cor = new ArrayList<Integer>();
         cor.clear();
+
+        textTimer = (TextView) findViewById(R.id.textTimer);
+
+        startButton = (Button) findViewById(R.id.btnStart);
+        startButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                startTime = SystemClock.uptimeMillis();
+                myHandler.postDelayed(updateTimerMethod, 1000);
+            }
+        });
+
+        pauseButton = (Button) findViewById(R.id.btnPause);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                timeSwap += timeInMillies;
+                myHandler.removeCallbacks(updateTimerMethod);
+            }
+        });
+
 
     }
 
@@ -59,10 +88,6 @@ public class Grafico extends Activity {
     }
 
     public void btnPressed(View view) {
-        //update the View
-        //Graf.setCircleColor(Color.GREEN);
-        //Graf.setLabelColor(Color.MAGENTA);
-        //Graf.setLabelText("Help");
 
         angulo.clear();
 
@@ -85,7 +110,28 @@ public class Grafico extends Activity {
         cor.add(Color.argb(255,0,0,255));
 
         Graf.set(cor, angulo);
-        //Graf.invalidate();
-        //Graf.requestLayout();
+
     }
+
+    private Runnable updateTimerMethod = new Runnable() {
+
+        public void run() {
+            timeInMillies = SystemClock.uptimeMillis()-startTime;
+            finalTime = timeSwap + timeInMillies;
+
+            int seconds = (int) (finalTime / 1000);
+            int minutes = seconds / 60;
+            int hours = minutes / 60;
+            seconds = seconds % 60;
+            hours = hours % 60;
+            //int milliseconds = (int) (finalTime % 1000);
+            textTimer.setText("" +String.format("%02d",hours) +":"
+                    +String.format("%02d",minutes)+":"
+                    +String.format("%02d",seconds));
+            myHandler.postDelayed(this, 1000);
+        }
+
+    };
+
+
 }
